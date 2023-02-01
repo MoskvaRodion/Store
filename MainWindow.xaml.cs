f7dca2a39e92c1ff64899c79fe6148e4a4ac1979
Rodion <moskovkinrodion2004@yandex.ru>
2023-02-01 15:13:41 +0300

    capcha

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,11 +24,34 @@ namespace Store
         public MainWindow()
         {
             InitializeComponent();
+            captchs();
+        }
+
+        private void captchs()
+        {
+            Random rand = new Random();
+            int num = rand.Next(6, 8);
+            string captcha = "";
+            int totl = 0;
+            do
+            {
+                int chr = rand.Next(48, 123);
+                if ((chr >= 48 && chr <= 58) || (chr >= 65 && chr <= 90) || (chr >= 97 && chr <= 122))
+                {
+                    captcha += (char)chr;
+                    totl++;
+                    if (totl == num)
+                        break;
+                    {
+                        
+                    }
+                }
+            } while (true);
+            capchaBlock.Text = captcha;
         }
 
         private void btn_login_Click(object sender, RoutedEventArgs e)
         {
-
             using (ApplicationContext db = new ApplicationContext())
             {
                 var currentUser = db.user.FirstOrDefault(p => p.login == Login.Text && p.password == password.Password);
@@ -36,7 +59,7 @@ namespace Store
                 var x = db.role.Where(p => p.id == User.CurrentUser.Role_id);
 
                 if (currentUser != null)
-                {
+                {                 
                     User.CurrentUser = currentUser;
                     var HomeForm = new home();
                     this.Hide();
@@ -54,5 +77,19 @@ namespace Store
             }
         }
 
+        private void btnCapcha_Click(object sender, RoutedEventArgs e)
+        {
+            var box = capchaBox.Text;
+            var block = capchaBlock.Text;
+            if (box == block)
+            {
+                MessageBox.Show("Капча пройдена");
+                btn_login.IsEnabled = true;
+            }
+            else
+            {
+                captchs();
+            }
+        }
     }
 }
